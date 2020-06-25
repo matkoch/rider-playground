bf3c54cd58996149c4e29e5fcb247eef79641b29
Matthias Koch <ithrowexceptions@gmail.com>
2020-06-25 14:25:25 +0200

    asd

--- Library/Editing/PostfixTemplates.cs
+++ Library/Editing/PostfixTemplates.cs
@@ -8,11 +8,12 @@ namespace Library.Editing
     public class PostfixTemplates
     {
         // FEAT: Postfix Templates
-        public string M(IEnumerable<int> numbers)
+        public IEnumerable<int> M(IEnumerable<int> numbers)
         {
             // Create StringBuilder (.var)
             // Iterate numbers (.foreach)
             // Check number even (.if)
+            // Return number (.yield)
 
             throw new NotImplementedException();
         }
